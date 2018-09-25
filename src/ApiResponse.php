bbffe55c068ea039d2832d50865cbb3cfe1f61a3
Ben Thomson <bennothommo@users.noreply.github.com>
2018-09-25 15:11:44 +0800

    Detect and throw exception for JSON errors in response

--- src/ApiResponse.php
+++ src/ApiResponse.php
@@ -3,6 +3,7 @@ namespace ABWebDevelopers\PinPayments;
 
 use Psr\Http\Message\ResponseInterface;
 use ABWebDevelopers\PinPayments\Endpoint\Exception\UnauthorizedException;
+use ABWebDevelopers\PinPayments\Endpoint\Exception\JSONException;
 
 class ApiResponse
 {
@@ -24,6 +25,37 @@ class ApiResponse
 
     public function data()
     {
-        return json_decode($this->response->getBody()->getContents(), true)['response'];
+        $data = json_decode($this->response->getBody()->getContents(), true);
+        $error = json_last_error();
+
+        if ($error !== JSON_ERROR_NONE) {
+            switch ($error) {
+                case JSON_ERROR_DEPTH:
+                    $errorString = 'The maximum stack depth has been exceeded';
+                    break;
+                case JSON_ERROR_STATE_MISMATCH:
+                    $errorString = 'Invalid or malformed JSON';
+                    break;
+                case JSON_ERROR_CTRL_CHAR:
+                    $errorString = 'Control character error, possibly incorrectly encoded';
+                    break;
+                case JSON_ERROR_SYNTAX:
+                    $errorString = 'Syntax error';
+                    break;
+                case JSON_ERROR_UTF8:
+                    $errorString = 'Malformed UTF-8 characters, possibly incorrectly encoded';
+                    break;
+                case JSON_ERROR_UTF16:
+                    $errorString = 'Malformed UTF-16 characters, possibly incorrectly encoded';
+                    break;
+                default:
+                    $errorString = 'Unknown JSON error';
+                    break;
+            }
+
+            throw new JSONException('Unable to decode response JSON: ' . $errorString);
+        }
+
+        return $data;
     }
 }

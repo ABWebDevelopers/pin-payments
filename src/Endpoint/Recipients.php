4af80c631e64c1c2dc8e644dd90d3cefab7b64a2
Ben Thomson <bennothommo@users.noreply.github.com>
2018-09-26 12:09:06 +0800

    Add all other available recipient API endpoints

--- src/Endpoint/Recipients.php
+++ src/Endpoint/Recipients.php
@@ -5,6 +5,7 @@ use ABWebDevelopers\PinPayments\ApiRequest;
 use ABWebDevelopers\PinPayments\Endpoint\Endpoint;
 use ABWebDevelopers\PinPayments\Endpoint\Exception\NotFoundException;
 use ABWebDevelopers\PinPayments\Entity\Recipient;
+use ABWebDevelopers\PinPayments\Entity\Transfer;
 
 class Recipients extends Endpoint
 {
@@ -73,17 +74,19 @@ class Recipients extends Endpoint
             $recipients = [];
 
             if ($response->isSuccessful()) {
-                foreach ($data['response'] as $recipient) {
-                    // Alias the bank account number if it is provided
-                    if (isset($recipient['bank_account']['number'])) {
-                        $recipient['bank_account']['display_number'] = $recipient['bank_account']['number'];
-                        unset($recipient['bank_account']['number']);
-                    }
+                if (count($data['response'])) {
+                    foreach ($data['response'] as $recipient) {
+                        // Alias the bank account number if it is provided
+                        if (isset($recipient['bank_account']['number'])) {
+                            $recipient['bank_account']['display_number'] = $recipient['bank_account']['number'];
+                            unset($recipient['bank_account']['number']);
+                        }
 
-                    $recipient = new Recipient($recipient);
-                    $recipient->setLoaded(true);
-                    $recipient->BankAccount->setLoaded(true);
-                    $recipients[] = $recipient;
+                        $recipient = new Recipient($recipient);
+                        $recipient->setLoaded(true);
+                        $recipient->BankAccount->setLoaded(true);
+                        $recipients[] = $recipient;
+                    }
                 }
             }
         } while ($data['pagination']['next'] !== null);
@@ -116,5 +119,85 @@ class Recipients extends Endpoint
         }
 
         return $recipient;
+    }
+
+    public function put(Recipient $recipient): Recipient
+    {
+        $data = $recipient->getApiData();
+
+        $request = new ApiRequest(
+            $this->client,
+            'PUT',
+            'recipients/' . $recipient->getToken(),
+            $data
+        );
+
+        $response = $request->send();
+        $data = $response->getResponseData();
+        $recipient->setSubmitted(true);
+
+        if ($response->isSuccessful()) {
+            // Alias the bank account number if it is provided
+            if (isset($data['response']['bank_account']['number'])) {
+                $data['response']['bank_account']['display_number'] = $data['response']['bank_account']['number'];
+                unset($data['response']['bank_account']['number']);
+            }
+
+            $recipient->set($data['response'])
+                ->setSuccessful(true)
+                ->setLoaded(true);
+        } elseif ($response->getStatusCode() === 404) {
+                throw new NotFoundException('No recipient could be found with the specified token.');
+        } else {
+            $recipient->setError($data['error_description'] ?? $data['error'] ?? 'Unknown error.')
+                ->setSuccessful(false)
+                ->setLoaded(false);
+
+            if (isset($data['messages'])) {
+                $recipient->setMessages($data['messages']);
+            }
+        }
+
+        return $recipient;
+    }
+
+    public function getTransfers(Recipient $recipient)
+    {
+        $page = 0;
+
+        do {
+            ++$page;
+
+            $request = new ApiRequest(
+                $this->client,
+                'GET',
+                'recipients/' . $recipient->getToken() . '/transfers'
+            );
+
+            $response = $request->send();
+            $data = $response->getResponseData();
+            $transfers = [];
+
+            if ($response->isSuccessful()) {
+                if (count($data['response'])) {
+                    foreach ($data['response'] as $transfer) {
+                        // Alias the bank account number if it is provided
+                        if (isset($transfer['bank_account']['number'])) {
+                            $transfer['bank_account']['display_number'] = $transfer['bank_account']['number'];
+                            unset($transfer['bank_account']['number']);
+                        }
+
+                        $transfer = new Transfer($transfer);
+                        $transfer->setLoaded(true);
+                        $transfer->BankAccount->setLoaded(true);
+                        $transfers[] = $transfer;
+                    }
+                }
+            } else {
+                throw new NotFoundException('No recipient could be found with the specified token.');
+            }
+        } while ($data['pagination']['next'] !== null);
+
+        return $transfers;
     }
 }
--- tests/TestCase/RecipientsEndpointTest.php
+++ tests/TestCase/RecipientsEndpointTest.php
@@ -3,6 +3,7 @@ namespace ABWebDevelopers\PinPayments\Tests\TestCase;
 
 use ABWebDevelopers\PinPayments\Entity\BankAccount;
 use ABWebDevelopers\PinPayments\Entity\Recipient;
+use ABWebDevelopers\PinPayments\Entity\Transfer;
 use ABWebDevelopers\PinPayments\Tests\TestCase\HttpTestCase;
 
 class RecipientsEndpointTest extends HttpTestCase
@@ -107,6 +108,169 @@ class RecipientsEndpointTest extends HttpTestCase
 
             $this->assertInstanceOf(BankAccount::class, $recipient->BankAccount);
             $this->assertTrue($recipient->BankAccount->isLoaded());
+        }
+
+        $this->assertEquals('XXXXX432', $recipients[0]->BankAccount->getNumber());
+        $this->assertEquals('XXXXX432', $recipients[1]->BankAccount->getNumber());
+    }
+
+    public function testGetRecipient()
+    {
+        $this->fakeResponse(200, '{
+            "response": {
+                "token": "rp_yeUyeNqDIhEEDRtH9mPvAQ",
+                "name": "Test Person",
+                "email": "test@test.com",
+                "created_at": "2018-09-25T09:15:59Z",
+                "bank_account": {
+                    "token": "ba_v5-1yD9B94e-S6OccKe7Eg",
+                    "name": "Mr Test Person",
+                    "bsb": "086366",
+                    "number": "XXXXX432",
+                    "bank_name": "National Australia Bank Limited",
+                    "branch": "Mount Lawley"
+                }
+            }
+        }');
+
+        $recipient = new Recipient('rp_yeUyeNqDIhEEDRtH9mPvAQ');
+        $recipient = $this->client->recipients->get($recipient);
+
+        $this->assertInstanceOf(Recipient::class, $recipient);
+        $this->assertTrue($recipient->isLoaded());
+
+        $this->assertInstanceOf(BankAccount::class, $recipient->BankAccount);
+        $this->assertTrue($recipient->BankAccount->isLoaded());
+        $this->assertEquals('XXXXX432', $recipient->BankAccount->getNumber());
+    }
+
+    public function testPutRecipient()
+    {
+        $this->fakeResponse(200, '{
+            "response": {
+                "token": "rp_JNZABDaziI4twLUeJrd7pw",
+                "name": "Test Person",
+                "email": "test@test.com",
+                "created_at": "2018-09-25T09:14:18Z",
+                "bank_account": {
+                    "token": "ba_F3z2mD42PW2sSQ5Zkr80Qg",
+                    "name": "Mr Test Person",
+                    "bsb": "086366",
+                    "number": "XXXXX432",
+                    "bank_name": "National Australia Bank Limited",
+                    "branch": "Mount Lawley"
+                }
+            }
+        }');
+
+        $recipient = new Recipient('rp_JNZABDaziI4twLUeJrd7pw');
+        $recipient = $this->client->recipients->get($recipient);
+
+        $this->fakeResponse(200, '{
+            "response": {
+                "token": "rp_JNZABDaziI4twLUeJrd7pw",
+                "name": "Test Person",
+                "email": "new@test.com",
+                "created_at": "2018-09-25T09:14:18Z",
+                "bank_account": {
+                    "token": "ba_F3z2mD42PW2sSQ5Zkr80Qg",
+                    "name": "Mr Test Person",
+                    "bsb": "086366",
+                    "number": "XXXXX678",
+                    "bank_name": "National Australia Bank Limited",
+                    "branch": "Mount Lawley"
+                }
+            }
+        }');
+
+        $recipient->setEmail('new@test.com');
+        $recipient->BankAccount->setNumber('12345678');
+        $recipient = $this->client->recipients->put($recipient);
+
+        $this->assertInstanceOf(Recipient::class, $recipient);
+        $this->assertTrue($recipient->isLoaded());
+        $this->assertTrue($recipient->isSubmitted());
+        $this->assertTrue($recipient->isSuccessful());
+        $this->assertEquals('new@test.com', $recipient->getEmail());
+
+        $this->assertInstanceOf(BankAccount::class, $recipient->BankAccount);
+        $this->assertTrue($recipient->BankAccount->isLoaded());
+        $this->assertEquals('XXXXX678', $recipient->BankAccount->getNumber());
+    }
+
+    public function testGetRecipientTransfers()
+    {
+        $this->fakeResponse(200, '{
+            "response": [
+                {
+                    "token": "tfer_lfUYEBK14zotCTykezJkfg",
+                    "status": "succeeded",
+                    "currency": "AUD",
+                    "description": "Test transfer",
+                    "amount": 1000,
+                    "total_debits": 0,
+                    "total_credits": 1000,
+                    "created_at": "2018-09-26T03:10:49Z",
+                    "paid_at": "2018-09-26T03:10:49Z",
+                    "reference": "Mr Test Person",
+                    "bank_account": {
+                        "token": "ba_F3z2mD42PW2sSQ5Zkr80Qg",
+                        "name": "Mr Test Person",
+                        "bsb": "086366",
+                        "number": "XXXXX432",
+                        "bank_name": "National Australia Bank Limited",
+                        "branch": "Mount Lawley"
+                    },
+                    "recipient": "rp_JNZABDaziI4twLUeJrd7pw"
+                },
+                {
+                    "token": "tfer_lc2x5lCzc7qoFSCi6ZFUfT",
+                    "status": "succeeded",
+                    "currency": "AUD",
+                    "description": "Test transfer 2",
+                    "amount": 2000,
+                    "total_debits": 0,
+                    "total_credits": 2000,
+                    "created_at": "2018-09-26T04:10:49Z",
+                    "paid_at": "2018-09-26T04:10:49Z",
+                    "reference": "Mr Test Person",
+                    "bank_account": {
+                        "token": "ba_F3z2mD42PW2sSQ5Zkr80Qg",
+                        "name": "Mr Test Person",
+                        "bsb": "086366",
+                        "number": "XXXXX432",
+                        "bank_name": "National Australia Bank Limited",
+                        "branch": "Mount Lawley"
+                    },
+                    "recipient": "rp_JNZABDaziI4twLUeJrd7pw"
+                }
+            ],
+            "count": 2,
+            "pagination": {
+                "current": 1,
+                "previous": null,
+                "next": null,
+                "per_page": 25,
+                "pages": 1,
+                "count": 2
+            }
+        }');
+
+        $recipient = new Recipient('rp_JNZABDaziI4twLUeJrd7pw');
+        $transfers = $this->client->recipients->getTransfers($recipient);
+
+        $this->assertCount(2, $transfers);
+
+        foreach ($transfers as $transfer) {
+            $this->assertInstanceOf(Transfer::class, $transfer);
+            $this->assertTrue($transfer->isLoaded());
+
+            $this->assertInstanceOf(BankAccount::class, $transfer->BankAccount);
+            $this->assertTrue($transfer->BankAccount->isLoaded());
+
+            $this->assertInstanceOf(Recipient::class, $transfer->Recipient);
+            $this->assertFalse($transfer->Recipient->isLoaded()); // Recipient isn't loaded as it is returned by the API as a simple token
+            $this->assertEquals('rp_JNZABDaziI4twLUeJrd7pw', $transfer->Recipient->getToken());
         }
     }
 }

825d1e69c7341fbefeb5dc0b3d8b863ec4dcbce0
Ben Thomson <ben@abweb.com.au>
2018-12-06 10:32:37 +0800

    Add refund entity

---
+++ src/Entity/Refund.php
@@ -0,0 +1,42 @@
+<?php
+namespace ABWebDevelopers\PinPayments\Entity;
+
+use ABWebDevelopers\PinPayments\Entity\Entity;
+
+class Refund extends Entity
+{
+    protected $attributes = [
+        // Request attributes
+        'token' => 'string',
+        'amount' => 'int',
+
+        // Response attributes
+        'currency' => 'string',
+        'charge' => 'ABWebDevelopers\PinPayments\Entity\Charge',
+        'created_at' => 'datetime',
+
+        'transfer' => 'array',
+        'amount_refunded' => 'int',
+        'total_fees' => 'int',
+        'captured' => 'bool',
+        'captured_at' => 'datetime',
+
+        // Communication attributes
+        'successful' => 'bool',
+        'loaded' => 'bool',
+        'submitted' => 'bool',
+        'status_message' => 'string',
+        'error' => 'string',
+        'messages' => 'array'
+    ];
+
+    protected $apiAttributes = [
+        'amount',
+        'charge',
+    ];
+
+    protected function onGetApiData($data = [], $associated = false)
+    {
+        return $data;
+    }
+}
